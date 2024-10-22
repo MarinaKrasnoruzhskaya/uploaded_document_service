2331dabe5a1a45018e9db624586f25df98a37165
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-10-22 23:37:37 +0300

    update change status in django admin

--- documents/admin.py
+++ documents/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.db import transaction
 
 from documents.models import Document
+from documents.tasks import send_notification
 
 
 @admin.register(Document)
@@ -13,3 +15,15 @@ class DocumentAdmin(admin.ModelAdmin):
         "status",
     )
 
+    @transaction.atomic
+    def save_model(self, request, obj, form, change):
+        """ Метод определяет задачу для отправки уведомления автору документа после проверки администратором """
+
+        if change:
+            old_status = Document.objects.get(pk=obj.pk).status
+            new_status = form.cleaned_data['status']
+
+        super().save_model(request, obj, form, change)
+
+        if old_status != new_status:
+            send_notification.delay(obj.id)
--- documents/views.py
+++ documents/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from rest_framework.permissions import IsAdminUser
+from rest_framework import mixins
 from rest_framework.viewsets import ModelViewSet
 
 from documents.models import Document
@@ -13,28 +12,12 @@ class DocumentViewSet(ModelViewSet):
 
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
-    http_method_names = ['post', 'patch', 'get']
+    http_method_names = ['post']
 
     def perform_create(self, serializer):
         """ Метод для автоматической привязки загруженного документа к авторизованному пользователю и
-        определения задачи для отправки уведомления администратору """
+        для определения задачи отправки уведомления администратору """
 
         serializer.save(author=self.request.user)
         id_ = serializer.data['id']  # Получаем ID нового документа для отправки письма
         send_notification.delay(id_)
-
-    def perform_update(self, serializer):
-        """ Метод определяет задачу для отправки уведомления автору документа после проверки администратором """
-
-        serializer.save()
-        status_ = serializer.data['status']
-        if status_ in ['document confirmed', 'document rejected']:
-            id_ = serializer.data['id']
-            send_notification.delay(id_)
-
-    def get_permissions(self):
-        """ Метод определяет доступ администратора к просмотру документа и изменению его статуса """
-
-        if self.action in ["update", "retrieve", "partial_update"]:
-            self.permission_classes = (IsAdminUser,)
-        return super().get_permissions()
