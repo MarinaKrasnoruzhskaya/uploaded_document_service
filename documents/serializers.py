784200d4f0603c028ad1ec72570429a9b84300c2
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-10-24 22:39:11 +0300

    fixes serializers

--- documents/serializers.py
+++ documents/serializers.py
@@ -11,7 +11,7 @@ class DocumentUploadSerializer(ModelSerializer):
 
     class Meta:
         model = Document
-        fields = ('document', 'status', 'author')
+        fields = ('document',)
 
 
 class FileViewSerializer(ModelSerializer):
